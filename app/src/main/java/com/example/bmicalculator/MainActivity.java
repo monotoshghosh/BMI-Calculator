cb78adb9ccb3e91f61e61385affe34a9bebf3761
monotoshghosh <monotoshghosh49@gmail.com>
2023-10-17 13:17:49 +0530

    Layout Complete

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //
+    EditText weight;
+    EditText height;
+    TextView result;
+    Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        weight=findViewById(R.id.weight);
+        height=findViewById(R.id.height);
+        result=findViewById(R.id.result);
+        button=findViewById(R.id.button);
     }
 }
