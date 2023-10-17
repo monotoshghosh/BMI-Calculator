61dbbd939de4466848e6b35682edbb1c5f9a22e7
monotoshghosh <monotoshghosh49@gmail.com>
2023-10-17 13:53:14 +0530

    Basic Done

--- app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.bmicalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,9 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
         weight=findViewById(R.id.weight);
         height=findViewById(R.id.height);
         result=findViewById(R.id.result);
         button=findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double w=Double.parseDouble(weight.getText().toString());
+                double h=Double.parseDouble(height.getText().toString());
+                double res = (w / (h * h));
+
+                result.setText("Your BMI is "+res);
+
+            }
+        });
+
+
     }
 }
